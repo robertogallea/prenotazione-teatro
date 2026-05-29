e864bd595e2062e9f1503b34478c66261fe766da
Roberto Gallea <write@robertogallea.com>
2026-05-29 17:59:12 +0200

    Auto login

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import inertia from '@inertiajs/vite';
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+// import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
@@ -27,8 +27,8 @@ export default defineConfig({
                 },
             },
         }),
-        wayfinder({
-            formVariants: true,
-        }),
+        // wayfinder({
+        //     formVariants: true,
+        // }),
     ],
 });
