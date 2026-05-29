ab6920f173c818e621036b46ffe5e13b285b387f
Roberto Gallea <write@robertogallea.com>
2026-05-29 17:38:19 +0200

    Remove seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create(['email' => 'test@example.com']);
         $this->call(SeatsSeeder::class);
     }
 }
